66ff026ab6bdd74448845b55f38ca94d5823bc6f
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-05-30 17:41:07 +0530

    error finding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" style={{backgroundColor:"#212121"}}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
